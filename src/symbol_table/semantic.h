cb3dd96a3d89e4265ff6dcb290ab9ff2707c988b
Rakibul Islam Rafi <168563308+CodeByRafi@users.noreply.github.com>
2026-07-27 22:34:08 +0600

    Refactor semantic.h to symbol_table.h with new struct

--- src/symbol_table/semantic.h
+++ src/symbol_table/semantic.h
@@ -1,5 +1,19 @@
-#ifndef SEMANTIC_H
-#define SEMANTIC_H
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
 #include "ast.h"
-int semantic_analyze(ASTNode *root);
+
+typedef struct Symbol {
+    char *name;
+    DataType type;
+    int scope_level;
+    int line_declared;
+    struct Symbol *next;
+} Symbol;
+
+void scope_enter(void);
+void scope_exit(void);
+int scope_declare(const char *name, DataType type, int line);
+Symbol *scope_lookup(const char *name);
+void print_symbol_table(void);
+
 #endif
